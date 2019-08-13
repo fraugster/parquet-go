e36eab10968e131dd388c2aee773989d24d19768
Andreas Krennmair <andreas@fraugster.com>
2019-08-13 16:29:04 +0200

    implement prototype of parquet schema parser.

---
+++ schema_parser_test.go
@@ -0,0 +1,53 @@
+package go_parquet
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSchemaParser(t *testing.T) {
+	testData := []struct {
+		Msg       string
+		ExpectErr bool
+	}{
+		{`message foo { }`, false},
+		{`message foo {`, true}, // missing closing brace
+		{`message foo { required int64 bar; }`, false},
+		{`message foo { repeated int64 bar; }`, false},
+		{`message foo { optional int64 bar; }`, false},
+		{`message foo { justwrong int64 bar; }`, true}, // incorrect repetition type
+		{`message foo { optional int64 bar }`, true},   // missing semicolon after column name
+		{`message foo { required binary the_id = 1; required binary client = 2; }`, false},
+		{`message foo { optional boolean is_fraud; }`, false},
+		{`message foo {
+			required binary the_id (STRING) = 1;
+			required binary client (STRING) = 2;
+			required binary request_body = 3;
+			required int64 ts = 4;
+			required group data_enriched (MAP) {
+				repeated group key_value (MAP_KEY_VALUE) {
+					required binary key = 5;
+					required binary value = 6;
+				}
+			}
+			optional boolean is_fraud = 7;
+		}`, false},
+		{`message $ { }`, true},                              // $ is not the start of a valid token.
+		{`message foo { optional int128 bar; }`, true},       // invalid type
+		{`message foo { optional int64 bar (BLUB); }`, true}, // invalid converted type
+	}
+
+	for idx, tt := range testData {
+		p := newSchemaParser(tt.Msg)
+		err := p.parse()
+
+		if tt.ExpectErr {
+			assert.Error(t, err, "%d. expected error, got none; parsed message: %s", idx, spew.Sdump(p.msg))
+		} else {
+			assert.NoError(t, err, "%d. expected no error, got error instead", idx)
+		}
+		t.Logf("%d. msg = %s expect err = %t err = %v ; parsed message: %s", idx, tt.Msg, tt.ExpectErr, err, spew.Sdump(p.msg))
+	}
+}
