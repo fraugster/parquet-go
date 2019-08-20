c12f4b4e2c21f4dd242e43debbffcee5c832f1be
Andreas Krennmair <andreas@fraugster.com>
2019-08-20 16:07:32 +0200

    floor: started code to fill struct with data.

--- floor/reader.go
+++ floor/reader.go
@@ -1,6 +1,11 @@
 package floor
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
 	goparquet "github.com/fraugster/parquet-go"
 )
 
@@ -86,8 +91,157 @@ func (r *Reader) init() {
 // structure of obj doesn't fit the data. obj needs to be
 // a pointer to an object.
 func (r *Reader) Scan(obj interface{}) error {
-	// TODO: implement
+	objValue := reflect.ValueOf(obj)
+
+	if objValue.Kind() != reflect.Ptr {
+		return errors.New("you didn't provide a pointer to an object") // TODO: improve error message
+	}
+
+	objValue = objValue.Elem()
+	if objValue.Kind() != reflect.Struct {
+		return errors.New("provided object is not a struct")
+	}
+
+	if err := fillStruct(objValue, r.data); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func fillStruct(value reflect.Value, data map[string]interface{}) error {
+	typ := value.Type()
+
+	numFields := typ.NumField()
+	for i := 0; i < numFields; i++ {
+		fieldValue := value.Field(i)
+
+		fieldName := strings.ToLower(typ.Field(i).Name)
+
+		fieldData, ok := data[fieldName]
+		if !ok {
+			// TODO: couple this somehow with schema and check whether field is required.
+			continue
+		}
+
+		if err := fillValue(fieldValue, fieldData); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func fillValue(value reflect.Value, data interface{}) error {
+	if value.Kind() == reflect.Ptr {
+		value.Set(reflect.New(value.Type().Elem()))
+		value = value.Elem()
+	}
+
+	switch value.Kind() {
+	case reflect.Bool:
+		b, err := getBoolValue(data)
+		if err != nil {
+			return err
+		}
+		value.SetBool(b)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		i, err := getIntValue(data)
+		if err != nil {
+			return err
+		}
+		value.SetInt(i)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		u, err := getUintValue(data)
+		if err != nil {
+			return err
+		}
+		value.SetUint(u)
+	case reflect.Float32, reflect.Float64:
+		f, err := getFloatValue(data)
+		if err != nil {
+			return err
+		}
+		value.SetFloat(f)
+	case reflect.Array, reflect.Slice:
+
+	case reflect.Map:
+
+	case reflect.String:
+		s, err := getStringValue(data)
+		if err != nil {
+			return err
+		}
+		value.SetString(s)
+	case reflect.Struct:
+
+	default:
+		return fmt.Errorf("unsupported type %s", value.Type())
+	}
+
+	return nil
+}
+
+func getBoolValue(data interface{}) (bool, error) {
+	b, ok := data.(bool)
+	if !ok {
+		return false, fmt.Errorf("expected bool, got %T", data)
+	}
+	return b, nil
+}
+
+func getStringValue(data interface{}) (string, error) {
+	s, ok := data.(string)
+	if !ok {
+		return "", fmt.Errorf("expected string, got %T", data)
+	}
+
+	return s, nil
+}
+
+func getIntValue(data interface{}) (int64, error) {
+	switch x := data.(type) {
+	case int:
+		return int64(x), nil
+	case int8:
+		return int64(x), nil
+	case int16:
+		return int64(x), nil
+	case int32:
+		return int64(x), nil
+	case int64:
+		return x, nil
+	default:
+		return 0, fmt.Errorf("expected integer, got %T", data)
+	}
+}
+
+func getUintValue(data interface{}) (uint64, error) {
+	switch x := data.(type) {
+	case uint:
+		return uint64(x), nil
+	case uint8:
+		return uint64(x), nil
+	case uint16:
+		return uint64(x), nil
+	case uint32:
+		return uint64(x), nil
+	case uint64:
+		return x, nil
+	default:
+		return 0, fmt.Errorf("expected unsigned integer, got %T", data)
+	}
+}
+
+func getFloatValue(data interface{}) (float64, error) {
+	switch x := data.(type) {
+	case float32:
+		return float64(x), nil
+	case float64:
+		return x, nil
+	default:
+		return 0, fmt.Errorf("expected float, got %T", data)
+	}
 }
 
 // Err returns an error in case Next returned false due to an error.
--- floor/reader_test.go
+++ floor/reader_test.go
@@ -4,12 +4,44 @@ import (
 	"os"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 	goparquet "github.com/fraugster/parquet-go"
+	"github.com/fraugster/parquet-go/parquet"
 )
 
 func TestReadFile(t *testing.T) {
-	rf, err := os.Open("files/snappy.parquet")
+	_ = os.Mkdir("files", 0755)
+
+	wf, err := os.OpenFile("files/readtest.parquet", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	require.NoError(t, err, "creating file failed")
+
+	w := goparquet.NewFileWriter(wf, goparquet.CompressionCodec(parquet.CompressionCodec_SNAPPY), goparquet.CreatedBy("floor-unittest"))
+
+	sd, err := goparquet.ParseSchemaDefinition(
+		`message test_msg {
+			required int64 foo;
+			optional binary bar (STRING);
+		}`)
+	require.NoError(t, err, "parsing schema definition failed")
+
+	t.Logf("schema definition: %s", spew.Sdump(sd))
+
+	w.SetSchemaDefinition(sd)
+
+	hlWriter := NewWriter(w)
+
+	type testMsg struct {
+		Foo int64
+		Bar *string
+	}
+
+	require.NoError(t, hlWriter.Write(testMsg{Foo: 1, Bar: strPtr("hello")}))
+	require.NoError(t, hlWriter.Write(testMsg{Foo: 23}))
+	require.NoError(t, hlWriter.Write(testMsg{Foo: 42, Bar: strPtr("world!")}))
+	require.NoError(t, hlWriter.Close())
+
+	rf, err := os.Open("files/readtest.parquet")
 	require.NoError(t, err)
 	defer rf.Close()
 
@@ -19,12 +51,22 @@ func TestReadFile(t *testing.T) {
 	hlReader := NewReader(reader)
 
 	count := 0
+
+	var result []testMsg
+
 	for hlReader.Next() {
+		var msg testMsg
+
+		require.NoError(t, hlReader.Scan(&msg), "%d. Scan failed", count)
 		//t.Logf("%d. data = %#v", count, hlReader.data)
+
+		result = append(result, msg)
+
 		count++
 	}
 
 	t.Logf("count = %d", count)
+	t.Logf("result = %s", spew.Sdump(result))
 
 	require.NoError(t, hlReader.Err(), "hlReader returned an error")
 }
