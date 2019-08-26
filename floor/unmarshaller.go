4788373db0b6e6736d570db07309d8f017da0ec0
Andreas Krennmair <andreas@fraugster.com>
2019-08-26 13:51:33 +0200

    floor: add missing unmarshaller files.

---
+++ floor/unmarshaller.go
@@ -0,0 +1,221 @@
+package floor
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	// ErrFieldNotFound indicates that the requested field could not be found.
+	ErrFieldNotFound = errors.New("field not found")
+
+	errFieldIsNotGroup = errors.New("field is not a group")
+)
+
+// Unmarshaller is the interface necessary for objects to
+// be unmarshalled.
+type Unmarshaller interface {
+	Unmarshal(obj UnmarshalObject) error
+}
+
+// UnmarshalObject is the interface an Unmarshaller needs to unmarshal its data
+// from.
+type UnmarshalObject interface {
+	GetField(field string) (UnmarshalElement, error)
+}
+
+// UnmarshalElement describes the interface to get the value of an element in an Unmarshaller
+// implementation.
+type UnmarshalElement interface {
+	Group() (UnmarshalObject, error)
+	Int32() (int32, error)
+	Int64() (int64, error)
+	Float32() (float32, error)
+	Float64() (float64, error)
+	Bool() (bool, error)
+	ByteArray() ([]byte, error)
+	List() (UnmarshalList, error)
+	Map() (UnmarshalMap, error)
+}
+
+// UnmarshalList describes the interface to get the values of a list in an Unmarshaller
+// implementation.
+type UnmarshalList interface {
+	Next() bool
+	Value() (UnmarshalElement, error)
+}
+
+// UnmarshalMap describes the interface to get key-value pairs of a map in an Unmarshaller
+// implementation.
+type UnmarshalMap interface {
+	Next() bool
+	Key() (UnmarshalElement, error)
+	Value() (UnmarshalElement, error)
+}
+
+func (o *object) GetField(field string) (UnmarshalElement, error) {
+	fieldData, ok := o.data[field]
+	if !ok {
+		return nil, ErrFieldNotFound
+	}
+
+	return &unmarshElem{data: fieldData}, nil
+}
+
+type unmarshElem struct {
+	data interface{}
+}
+
+func (e *unmarshElem) Group() (UnmarshalObject, error) {
+	data, ok := e.data.(map[string]interface{})
+	if !ok {
+		return nil, errFieldIsNotGroup
+	}
+
+	return &object{data: data}, nil
+}
+
+func (e *unmarshElem) Int32() (int32, error) {
+	i, ok := e.data.(int32)
+	if !ok {
+		return 0, fmt.Errorf("expected int32, found %T instead", e.data)
+	}
+	return i, nil
+}
+
+func (e *unmarshElem) Int64() (int64, error) {
+	i, ok := e.data.(int64)
+	if !ok {
+		return 0, fmt.Errorf("expected int64, found %T instead", e.data)
+	}
+	return i, nil
+}
+
+func (e *unmarshElem) Float32() (float32, error) {
+	f, ok := e.data.(float32)
+	if !ok {
+		return 0, fmt.Errorf("expected float32, found %T instead", e.data)
+	}
+	return f, nil
+}
+
+func (e *unmarshElem) Float64() (float64, error) {
+	f, ok := e.data.(float64)
+	if !ok {
+		return 0, fmt.Errorf("expected float64, found %T instead", e.data)
+	}
+	return f, nil
+}
+
+func (e *unmarshElem) Bool() (bool, error) {
+	f, ok := e.data.(bool)
+	if !ok {
+		return false, fmt.Errorf("expected bool, found %T instead", e.data)
+	}
+	return f, nil
+}
+
+func (e *unmarshElem) ByteArray() ([]byte, error) {
+	f, ok := e.data.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("expected []byte, found %T instead", e.data)
+	}
+	return f, nil
+}
+
+func (e *unmarshElem) List() (UnmarshalList, error) {
+	data, ok := e.data.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("data is not a list, found %T instead", e.data)
+	}
+
+	listData, ok := data["list"]
+	if !ok {
+		return nil, errors.New("sub-group list not found")
+	}
+
+	elemList, ok := listData.([]map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("expected sub-group list to be []map[string]interface{}, got %T instead", listData)
+	}
+
+	return &unmarshList{list: elemList, idx: -1}, nil
+}
+
+func (e *unmarshElem) Map() (UnmarshalMap, error) {
+	data, ok := e.data.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("data is not a map, found %T instead", e.data)
+	}
+
+	kvData, ok := data["key_value"]
+	if !ok {
+		return nil, errors.New("sub-group key_value not found")
+	}
+
+	kvList, ok := kvData.([]map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("expected sub-group key_value to be []map[string]interface{}, got %T instead", kvData)
+	}
+
+	return &unmarshMap{data: kvList, idx: -1}, nil
+}
+
+type unmarshList struct {
+	list []map[string]interface{}
+	idx  int
+}
+
+func (l *unmarshList) Next() bool {
+	l.idx++
+	return l.idx < len(l.list)
+}
+
+func (l *unmarshList) Value() (UnmarshalElement, error) {
+	if l.idx >= len(l.list) {
+		return nil, errors.New("iterator has reached end of list")
+	}
+
+	elem, ok := l.list[l.idx]["element"]
+	if !ok {
+		return nil, errors.New("element not found in current list element")
+	}
+
+	return &unmarshElem{data: elem}, nil
+}
+
+type unmarshMap struct {
+	data []map[string]interface{}
+	idx  int
+}
+
+func (m *unmarshMap) Next() bool {
+	m.idx++
+	return m.idx < len(m.data)
+}
+
+func (m *unmarshMap) Key() (UnmarshalElement, error) {
+	if m.idx >= len(m.data) {
+		return nil, errors.New("iterator has reached end of map")
+	}
+
+	elem, ok := m.data[m.idx]["key"]
+	if !ok {
+		return nil, errors.New("key not found in current map element")
+	}
+
+	return &unmarshElem{data: elem}, nil
+}
+
+func (m *unmarshMap) Value() (UnmarshalElement, error) {
+	if m.idx >= len(m.data) {
+		return nil, errors.New("iterator has reached end of map")
+	}
+
+	elem, ok := m.data[m.idx]["value"]
+	if !ok {
+		return nil, errors.New("value not found in current map element")
+	}
+
+	return &unmarshElem{data: elem}, nil
+}
---
+++ floor/unmarshaller_test.go
@@ -0,0 +1,252 @@
+package floor
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestObjectUnmarshalling(t *testing.T) {
+	obj := newObjectWithData(map[string]interface{}{
+		"foo":  int64(23),
+		"bar":  int32(42),
+		"baz":  true,
+		"name": []byte("John Doe"),
+		"my_group": map[string]interface{}{
+			"foo1": float32(23.5),
+			"bar1": float64(9000.5),
+		},
+		"id_list": map[string]interface{}{
+			"list": []map[string]interface{}{
+				map[string]interface{}{
+					"element": int64(1),
+				},
+				map[string]interface{}{
+					"element": int64(2),
+				},
+				map[string]interface{}{
+					"element": int64(15),
+				},
+				map[string]interface{}{
+					"element": int64(28),
+				},
+				map[string]interface{}{
+					"element": int64(32),
+				},
+			},
+		},
+		"data_map": map[string]interface{}{
+			"key_value": []map[string]interface{}{
+				map[string]interface{}{
+					"key":   []byte("data0"),
+					"value": int32(0),
+				},
+				map[string]interface{}{
+					"key":   []byte("data1"),
+					"value": int32(1),
+				},
+				map[string]interface{}{
+					"key":   []byte("data2"),
+					"value": int32(2),
+				},
+				map[string]interface{}{
+					"key":   []byte("data3"),
+					"value": int32(3),
+				},
+				map[string]interface{}{
+					"key":   []byte("data4"),
+					"value": int32(4),
+				},
+			},
+		},
+		"nested_data_map": map[string]interface{}{
+			"key_value": []map[string]interface{}{
+				map[string]interface{}{
+					"key": int64(23),
+					"value": map[string]interface{}{
+						"foo": int32(42),
+					},
+				},
+			},
+		},
+	})
+
+	elem, err := obj.GetField("foo")
+	require.NoError(t, err, "getting foo failed")
+	i64, err := elem.Int64()
+	require.NoError(t, err, "getting foo as int64 failed")
+	require.Equal(t, int64(23), i64)
+
+	elem, err = obj.GetField("bar")
+	require.NoError(t, err, "getting bar failed")
+	i32, err := elem.Int32()
+	require.NoError(t, err, "getting bar as int32 failed")
+	require.Equal(t, int32(42), i32)
+
+	elem, err = obj.GetField("baz")
+	require.NoError(t, err, "getting baz failed")
+	b, err := elem.Bool()
+	require.NoError(t, err, "getting baz as bool failed")
+	require.Equal(t, true, b)
+
+	elem, err = obj.GetField("my_group")
+	require.NoError(t, err, "getting my_group failed")
+	myGroup, err := elem.Group()
+	require.NoError(t, err, "getting my_group as group failed")
+
+	elem, err = myGroup.GetField("foo1")
+	require.NoError(t, err, "getting my_group.foo1 failed")
+	f32, err := elem.Float32()
+	require.NoError(t, err, "getting my_group.foo1 as float32 failed")
+	require.Equal(t, float32(23.5), f32)
+
+	elem, err = myGroup.GetField("bar1")
+	require.NoError(t, err, "getting my_group.bar1 failed")
+	f64, err := elem.Float64()
+	require.NoError(t, err, "getting my_group.bar1 as float64 failed")
+	require.Equal(t, float64(9000.5), f64)
+
+	elem, err = obj.GetField("id_list")
+	require.NoError(t, err, "getting id_list failed")
+	idList, err := elem.List()
+	require.NoError(t, err, "getting id_list as list failed")
+
+	var i64List []int64
+
+	for idList.Next() {
+		v, err := idList.Value()
+		require.NoError(t, err, "getting list value failed")
+		i64, err := v.Int64()
+		require.NoError(t, err, "getting list value as int64 failed")
+		i64List = append(i64List, i64)
+	}
+
+	require.Equal(t, []int64{1, 2, 15, 28, 32}, i64List, "list id_list values don't match")
+
+	elem, err = obj.GetField("data_map")
+	require.NoError(t, err, "getting data_map failed")
+	dataMap, err := elem.Map()
+	require.NoError(t, err, "getting data_map as map failed")
+
+	mapData := make(map[string]int32)
+
+	for dataMap.Next() {
+		key, err := dataMap.Key()
+		require.NoError(t, err, "getting key from map failed")
+		keyData, err := key.ByteArray()
+		require.NoError(t, err, "getting key as []byte failed")
+
+		value, err := dataMap.Value()
+		require.NoError(t, err, "getting value from map failed")
+		valueData, err := value.Int32()
+		require.NoError(t, err, "getting value as int32 failed")
+
+		mapData[string(keyData)] = valueData
+	}
+
+	require.Equal(t, map[string]int32{"data0": 0, "data1": 1, "data2": 2, "data3": 3, "data4": 4}, mapData)
+}
+
+func TestObjectUnmarshallingErrors(t *testing.T) {
+	obj := newObjectWithData(map[string]interface{}{
+		"foo":          int64(23),
+		"bar":          int32(42),
+		"invalid_list": map[string]interface{}{},
+		"invalid_list_2": map[string]interface{}{
+			"list": map[string]interface{}{"foo": int32(0)},
+		},
+		"invalid_list_element": map[string]interface{}{
+			"list": []map[string]interface{}{
+				map[string]interface{}{"foo": int32(0)},
+			},
+		},
+		"invalid_map": map[string]interface{}{},
+		"invalid_map_2": map[string]interface{}{
+			"key_value": map[string]interface{}{"foo": int32(0)},
+		},
+
+		"data_map_no_keyvalues": map[string]interface{}{
+			"key_value": []map[string]interface{}{
+				map[string]interface{}{},
+			},
+		},
+	})
+
+	_, err := obj.GetField("does_not_exist")
+	require.Error(t, err)
+
+	elem, err := obj.GetField("foo")
+	require.NoError(t, err)
+	_, err = elem.Bool()
+	require.Error(t, err)
+	_, err = elem.ByteArray()
+	require.Error(t, err)
+	_, err = elem.Float32()
+	require.Error(t, err)
+	_, err = elem.Float64()
+	require.Error(t, err)
+	_, err = elem.Int32()
+	require.Error(t, err)
+	_, err = elem.Group()
+	require.Error(t, err)
+	_, err = elem.List()
+	require.Error(t, err)
+	_, err = elem.Map()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("bar")
+	require.NoError(t, err)
+	_, err = elem.Int64()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("invalid_list")
+	require.NoError(t, err)
+	_, err = elem.List()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("invalid_list_2")
+	require.NoError(t, err)
+	_, err = elem.List()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("invalid_list_element")
+	require.NoError(t, err)
+	list, err := elem.List()
+
+	for list.Next() {
+		_, err := list.Value()
+		require.Error(t, err)
+	}
+
+	_, err = list.Value()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("invalid_map")
+	require.NoError(t, err)
+	_, err = elem.Map()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("invalid_map_2")
+	require.NoError(t, err)
+	_, err = elem.Map()
+	require.Error(t, err)
+
+	elem, err = obj.GetField("data_map_no_keyvalues")
+	require.NoError(t, err)
+	dataMap, err := elem.Map()
+	require.NoError(t, err)
+
+	for dataMap.Next() {
+		_, err := dataMap.Key()
+		require.Error(t, err)
+
+		_, err = dataMap.Value()
+		require.Error(t, err)
+	}
+
+	_, err = dataMap.Key()
+	require.Error(t, err)
+
+	_, err = dataMap.Value()
+	require.Error(t, err)
+}
