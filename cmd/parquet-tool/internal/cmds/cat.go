f89371d5746596943e75371368547977a9e211e4
Forud <fzerorubigd@gmail.com>
2019-09-03 14:42:35 +0200

    parquet-tools: add support for head command

--- cmd/parquet-tool/internal/cmds/cat.go
+++ cmd/parquet-tool/internal/cmds/cat.go
@@ -1,13 +1,10 @@
 package cmds
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-	goparquet "github.com/fraugster/parquet-go"
 )
 
 func init() {
@@ -24,69 +21,9 @@ var catCmd = &cobra.Command{
 			_ = cmd.Usage()
 			os.Exit(1)
 		}
-		fl, err := os.Open(args[0])
-		if err != nil {
-			log.Fatalf("Can not open the file: %q", err)
-		}
-		defer fl.Close()
 
-		reader, err := goparquet.NewFileReader(fl)
-		if err != nil {
-			// TODO: using fatal actually by-pass the defer, do it better
-			log.Fatalf("Failed to read the parquet header: %q", err)
-		}
-
-		for {
-			err := reader.ReadRowGroup()
-			if err != nil && err != io.EOF {
-				log.Fatalf("Failed to read the row group: %q", err)
-			}
-
-			if err == io.EOF {
-				return
-			}
-
-			for i := int64(0); i < reader.NumRecords(); i++ {
-				data, err := reader.GetData()
-				if err != nil {
-					log.Printf("Reading data failed with error, skip current row group: %q", err)
-					break
-				}
-
-				printData(data, "")
-				fmt.Println()
-			}
+		if err := catFile(args[0], -1); err != nil {
+			log.Fatal(err)
 		}
 	},
-}
-
-func printPrimitive(ident, name string, v interface{}) {
-	fmt.Println(ident + name + " = " + fmt.Sprint(v))
-}
-
-func printData(m map[string]interface{}, ident string) {
-	for i := range m {
-		switch t := m[i].(type) {
-		case map[string]interface{}:
-			fmt.Println(ident + i + ":")
-			printData(t, ident+".")
-		case []map[string]interface{}:
-			for j := range t {
-				fmt.Println(ident + i + ":")
-				printData(t[j], ident+".")
-			}
-		case []byte:
-			fmt.Println(ident + i + " = " + string(t))
-		case [][]byte:
-			for j := range t {
-				fmt.Println(ident + i + " = " + string(t[j]))
-			}
-		case []interface{}:
-			for j := range t {
-				fmt.Println(ident + i + " = " + fmt.Sprint(t[j]))
-			}
-		default:
-			printPrimitive(ident, i, t)
-		}
-	}
 }
