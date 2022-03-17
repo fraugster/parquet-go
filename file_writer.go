0cad81033e987c5b5bd5af55fc783e37f614fb97
Brad Melin <brad@fraugster.com>
2022-03-17 13:57:17 +0100

    file_writer: return nil during flush if there are no records

--- file_writer.go
+++ file_writer.go
@@ -3,7 +3,6 @@ package goparquet
 import (
 	"context"
 	"encoding/binary"
-	"errors"
 	"io"
 
 	"github.com/fraugster/parquet-go/parquet"
@@ -231,7 +230,7 @@ func (fw *FileWriter) FlushRowGroup(opts ...FlushRowGroupOption) error {
 func (fw *FileWriter) FlushRowGroupWithContext(ctx context.Context, opts ...FlushRowGroupOption) error {
 	// Write the entire row group
 	if fw.schemaWriter.rowGroupNumRecords() == 0 {
-		return errors.New("nothing to write")
+		return nil
 	}
 
 	if fw.w.Pos() == 0 {
