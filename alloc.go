b59b1b0dfc7f79ffcbc0b459797a2e022c6142bc
Andreas Krennmair <andreas@fraugster.com>
2022-05-08 14:56:14 +0200

    Add missing file.

---
+++ alloc.go
@@ -0,0 +1,76 @@
+package goparquet
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type allocTracker struct {
+	allocs    map[interface{}]uint64
+	totalSize uint64
+	maxSize   uint64
+}
+
+func newAllocTracker(maxSize uint64) *allocTracker {
+	return &allocTracker{
+		allocs:  make(map[interface{}]uint64),
+		maxSize: maxSize,
+	}
+}
+
+func (t *allocTracker) register(obj interface{}, size uint64) {
+	if t == nil {
+		return
+	}
+
+	if _, ok := t.allocs[obj]; ok { // object has already been tracked, no need to add it.
+		return
+	}
+
+	t.allocs[obj] = size
+	t.totalSize += size
+
+	runtime.SetFinalizer(obj, t.finalize)
+
+	if t.maxSize > 0 && t.totalSize > t.maxSize {
+		t.doPanic(t.totalSize)
+	}
+}
+
+func (t *allocTracker) test(size uint64) {
+	if t == nil {
+		return
+	}
+	if t.maxSize > 0 && t.totalSize+size > t.maxSize {
+		t.doPanic(t.totalSize + size)
+	}
+}
+
+func (t *allocTracker) doPanic(totalSize uint64) {
+	if t == nil {
+		return
+	}
+	panic(fmt.Sprintf("memory usage of %d bytes is greater than configured maximum of %d bytes", totalSize, t.maxSize))
+}
+
+func (t *allocTracker) finalize(obj interface{}) {
+	if t == nil {
+		return
+	}
+
+	size, ok := t.allocs[obj]
+	if !ok { // if object hasn't been tracked, do nothing.
+		return
+	}
+
+	// remove size from total size, and unregister from tracker.
+	t.totalSize -= size
+	delete(t.allocs, obj)
+}
+
+func (t *allocTracker) getTotalSize() uint64 {
+	if t == nil {
+		return 0
+	}
+	return t.totalSize
+}
